77583ef057c90d7c316caec52709c8b8a6ee76dc
Giovanny Pacheco <blacklofox@gmail.com>
2021-06-28 21:22:10 -0400

    Adding dynamic functionality with JS

--- js/app.js
+++ js/app.js
@@ -1,47 +1,109 @@
-const income = [
-    new Income("Salary", 1050000.00),
-    new Income("Profit per stock market", 1000000.00)
-];
+const income = [new Income("Salary", 1050000.0)];
 
 const expenses = [
-    new Expenses("Apartment rent payment", 250000.00),
-    new Expenses("Amenities payout", 100000.00),
-    new Expenses("Credit card payment", 200000.00)
+  new Expenses("Apartment rent payment", 243846.0),
+  new Expenses("Amenities payout", 94012.0),
+  new Expenses("Credit card payment", 200000.0),
+  new Expenses("Mom's salary", 100000.0),
+  new Expenses("Personal expenses", 100000.0),
 ];
 
 let loadApp = () => {
-    loadHeader();
-}
+  loadHeader();
+  loadIncome();
+  loadExpenses();
+};
 
 let totalIncome = () => {
-    let totalIncom = 0;
-    for(let incom of income){
-        totalIncom += incom.value;
-    }
-    return totalIncom;
-}
+  let totalIncom = 0;
+  for (let incom of income) {
+    totalIncom += incom.value;
+  }
+  return totalIncom;
+};
 
 let totalExpenses = () => {
-    let totalExpense = 0;
-    for(let expense of expenses){
-        totalExpense += expense.value;
-    }
-    return totalExpense;
-}
+  let totalExpense = 0;
+  for (let expense of expenses) {
+    totalExpense += expense.value;
+  }
+  return totalExpense;
+};
 
 let loadHeader = () => {
-    let budget = totalIncome() - totalExpenses();
-    let percentageExpenses = totalExpenses()/totalIncome();
-    document.getElementById("budget").innerHTML = currencyFormat(budget);
-    document.getElementById("percentage").innerHTML = percentageFormat(percentageExpenses);
-    document.getElementById("income").innerHTML = currencyFormat(totalIncome());
-    document.getElementById("expenses").innerHTML = currencyFormat(totalExpenses());
-}
+  let budget = totalIncome() - totalExpenses();
+  let percentageExpenses = totalExpenses() / totalIncome();
+  document.getElementById("budget").innerHTML = currencyFormat(budget);
+  document.getElementById("percentage").innerHTML =
+    percentageFormat(percentageExpenses);
+  document.getElementById("income").innerHTML = currencyFormat(totalIncome());
+  document.getElementById("expenses").innerHTML = currencyFormat(
+    totalExpenses()
+  );
+};
 
 const currencyFormat = (value) => {
-    return value.toLocaleString("en-US", {style:"currency", currency:"USD", minimumFractionDigits:4});
-}
+  return value.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
 
 const percentageFormat = (value) => {
-    return value.toLocaleString("en-US", {style:"percent", minimumFractionDigits:2});
-}
+  return value.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
+
+const loadIncome = () => {
+  let incomeHTML = "";
+  for (let incom of income) {
+    incomeHTML += createIncomeHtml(incom);
+  }
+  document.getElementById("income-list").innerHTML = incomeHTML;
+};
+
+const createIncomeHtml = (income) => {
+  let incomeHTML = `
+    <div class="element cleanStyles">
+    <div class="element_description">${income.description}</div>
+    <div class="right cleanStyles">
+      <div class="element_value">+ ${currencyFormat(income.value)}</div>
+      <div class="element_delete">
+        <button class="element_delete--btn">
+          <ion-icon name="close-circle-outline"></ion-icon>
+        </button>
+      </div>
+    </div>
+  </div>
+    `;
+  return incomeHTML;
+};
+
+const loadExpenses = () => {
+  let expenseHTML = "";
+  for (let expense of expenses) {
+    expenseHTML += createExpensesHtml(expense);
+  }
+  document.getElementById("expenses-list").innerHTML = expenseHTML;
+};
+
+const createExpensesHtml = (expense) => {
+  let expenseHTML = `
+      <div class="element cleanStyles">
+      <div class="element_description">${expense.description}</div>
+      <div class="right cleanStyles">
+        <div class="element_value">- ${currencyFormat(expense.value)}</div>
+        <div class="element_percentage"> ${percentageFormat(expense.value/totalExpenses())}</div>
+        <div class="element_delete">
+          <button class="element_delete--btn">
+            <ion-icon name="close-circle-outline"></ion-icon>
+          </button>
+        </div>
+      </div>
+    </div>
+      `;
+  return expenseHTML;
+};
